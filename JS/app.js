9d8353f9226f32c2ba1e22f513aec713b6d783a1
Marcel Williams <marcelwilliams503@gmail.com>
2017-03-27 19:06:40 -0700

    Update app.js

--- JS/app.js
+++ JS/app.js
@@ -1,8 +1,4 @@
 // This file sets up the event listeners and functions for the javascript funtionality
-<<<<<<< HEAD
-=======
-
-
 function openNav() {
   document.getElementById("mySidenav").style.width = "200px";
 }
@@ -10,4 +6,3 @@ function openNav() {
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
 }
->>>>>>> f0a180d0c23bd57848199b8e4db742864f1b8d9f
