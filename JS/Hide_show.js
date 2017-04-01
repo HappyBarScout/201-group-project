75338e6792ed4f9bff93dd155f2472dd1f6095ea
oscartzintzun <oscartzintzun123@gmail.com>
2017-03-31 22:52:33 -0700

    fixed show hide

--- JS/Hide_show.js
+++ JS/Hide_show.js
@@ -1,12 +1,138 @@
 function showHidden() {
-    console.log('it ran: ');
-    if (document.getElementById('knock').getAttribute('class') === "item") {
-        document.getElementById('knock').setAttribute('class', 'Hide');
-    } else {
-        document.getElementById('knock').setAttribute('class', 'item');
+  console.log('it ran: ');
+
+  if (document.getElementById('knock').getAttribute('class') === "item") {
+    document.getElementById('knock').setAttribute('class', 'Hide');
+  } else {
+    document.getElementById('knock').setAttribute('class', 'item');
+  };
+
+  if (document.getElementById('bang').getAttribute('class') === "item") {
+    document.getElementById('bang').setAttribute('class', 'Hide');
+  } else {
+    document.getElementById('bang').setAttribute('class', 'item');
+  };
+
+  if (document.getElementById('aviary').getAttribute('class') === "item") {
+    document.getElementById('aviary').setAttribute('class', 'Hide');
+  } else {
+    document.getElementById('aviary').setAttribute('class', 'item');
+  };
+
+  if (document.getElementById('interurban').getAttribute('class') === "item") {
+    document.getElementById('interurban').setAttribute('class', 'Hide');
+  } else {
+    document.getElementById('interurban').setAttribute('class', 'item');
+  };
+
+  if (document.getElementById('raes').getAttribute('class') === "item") {
+    document.getElementById('raes').setAttribute('class', 'Hide');
+  } else {
+    document.getElementById('raes').setAttribute('class', 'item');
+  };
+
+  if (document.getElementById('paleys').getAttribute('class') === "item") {
+    document.getElementById('paleys').setAttribute('class', 'Hide');
+  } else {
+    document.getElementById('paleys').setAttribute('class', 'item');
+  };
+
+  if (document.getElementById('bartini').getAttribute('class') === "item") {
+    document.getElementById('bartini').setAttribute('class', 'Hide');
+  } else {
+    document.getElementById('bartini').setAttribute('class', 'item');
+  };
+
+  if (document.getElementById('ringside').getAttribute('class') === "item") {
+    document.getElementById('ringside').setAttribute('class', 'Hide');
+  } else {
+    document.getElementById('ringside').setAttribute('class', 'item');
+  };
+
+  if (document.getElementById('teardrop').getAttribute('class') === "item") {
+    document.getElementById('teardrop').setAttribute('class', 'Hide');
+  } else {
+    document.getElementById('teardrop').setAttribute('class', 'item');
+  };
+
+  if (document.getElementById('clyde').getAttribute('class') === "item") {
+    document.getElementById('clyde').setAttribute('class', 'Hide');
+  } else {
+    document.getElementById('clyde').setAttribute('class', 'item');
+  };
+
+  if (document.getElementById('imperial').getAttribute('class') === "item") {
+    document.getElementById('imperial').setAttribute('class', 'Hide');
+  } else {
+    document.getElementById('imperial').setAttribute('class', 'item');
+  };
+
+  if (document.getElementById('sauce').getAttribute('class') === "item") {
+    document.getElementById('sauce').setAttribute('class', 'Hide');
+  } else {
+    document.getElementById('sauce').setAttribute('class', 'item');
+  };
+
+  if (document.getElementById('trifecta').getAttribute('class') === "item") {
+    document.getElementById('trifecta').setAttribute('class', 'Hide');
+  } else {
+    document.getElementById('trifecta').setAttribute('class', 'item');
+  };
+
+  if (document.getElementById('tapalaya').getAttribute('class') === "item") {
+    document.getElementById('tapalaya').setAttribute('class', 'Hide');
+  } else {
+    document.getElementById('tapalaya').setAttribute('class', 'item');
+  };
+
+  if (document.getElementById('aalto').getAttribute('class') === "item") {
+    document.getElementById('aalto').setAttribute('class', 'Hide');
+  } else {
+    document.getElementById('aalto').setAttribute('class', 'item');
+  };
+
+  if (document.getElementById('dots').getAttribute('class') === "item") {
+    document.getElementById('dots').setAttribute('class', 'Hide');
+  } else {
+    document.getElementById('dots').setAttribute('class', 'item');
+  };
+
+  if (document.getElementById('slingshot').getAttribute('class') === "item") {
+    document.getElementById('slingshot').setAttribute('class', 'Hide');
+  } else {
+    document.getElementById('slingshot').setAttribute('class', 'item');
   };
 
 };
+
+
+
+
+document.getElementById('bang').setAttribute('class', 'hide');
+document.getElementById('knock').setAttribute('class', 'hide');
+document.getElementById('aviary').setAttribute('class', 'hide');
+document.getElementById('interurban').setAttribute('class', 'hide');
+document.getElementById('raes').setAttribute('class', 'hide');
+document.getElementById('paleys').setAttribute('class', 'hide');
+document.getElementById('bartini').setAttribute('class', 'hide');
+document.getElementById('ringside').setAttribute('class', 'hide');
+document.getElementById('teardrop').setAttribute('class', 'hide');
+document.getElementById('clyde').setAttribute('class', 'hide');
+document.getElementById('imperial').setAttribute('class', 'hide');
+document.getElementById('sauce').setAttribute('class', 'hide');
+document.getElementById('trifecta').setAttribute('class', 'hide');
+document.getElementById('tapalaya').setAttribute('class', 'hide');
+document.getElementById('aalto').setAttribute('class', 'hide');
+document.getElementById('dots').setAttribute('class', 'hide');
+document.getElementById('slingshot').setAttribute('class', 'hide');
+
+
+
+
+
+
+
+
 
 
 
